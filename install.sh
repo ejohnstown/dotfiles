7723f4d0ac3ab3179a1efaeb74175245a6b56876
John Safranek <john@wolfssl.com>
2020-11-03 14:14:48 -0800

    add a couple options to vimrc

--- install.sh
+++ install.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
-echo "Hello, install."
+install_vimrc() {
+    echo "installing vimrc"
+}
+
+install_vimplugins() {
+    echo "installing vim plugins"
+}
+
+install_git() {
+    echo "installing gitrc"
+}
+
+install_tmux() {
+    echo "installing tmuxrc"
+}
+
+case "$1" in
+    vim)
+        install_vimrc
+        install_vimplugins
+        ;;
+    git)
+        install_git
+        ;;
+    tmux)
+        install_tmux
+        ;;
+    all)
+        install_vimrc
+        install_vimplugins
+        install_git
+        install_tmux
+        ;;
+    *)
+      echo "Usage: $(basename "$0") {vim|git|tmux|all}"
+      ;;
+esac
